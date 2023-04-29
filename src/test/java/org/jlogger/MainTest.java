8ad2cfb26391e93502a556818621120528b887b4
Shahnawaz Khan <mailtoshahnawazk@gmail.com>
2023-04-29 22:47:33 +0530

    Added tests

---
+++ src/test/java/org/jlogger/MainTest.java
@@ -0,0 +1,16 @@
+package org.jlogger;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void call() {
+    }
+}
