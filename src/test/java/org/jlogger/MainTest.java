a8a09682fc26df5aa38593182af3224d605fcb9f
Shahnawaz Khan <mailtoshahnawazk@gmail.com>
2023-05-04 14:07:15 +0530

    Added jacoco reports in build scripts

--- src/test/java/org/jlogger/MainTest.java
+++ src/test/java/org/jlogger/MainTest.java
@@ -1,14 +1,20 @@
 package org.jlogger;
 
 import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
     @Test
     void main() {
+
     }
 
     @Test
     void call() {
+        int result = new CommandLine(new Main()).execute();
+        assertEquals(0, result);
     }
 }
