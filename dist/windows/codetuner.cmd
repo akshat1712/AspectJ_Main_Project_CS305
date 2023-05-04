f669572719e1b134191a846033d5bebdee6a52e8
Shahnawaz Khan <mailtoshahnawazk@gmail.com>
2023-05-04 20:15:06 +0530

    Fixed a bug in windows cmd file

--- dist/windows/codetuner.cmd
+++ dist/windows/codetuner.cmd
@@ -1,5 +1,5 @@
-@echo off
+@echo on
 set "myPath=%~dp0"
-set "jarPath=\codetuner-1.0-SNAPSHOT-all.jar"
+set "jarPath=codetuner-1.0-SNAPSHOT-all.jar"
 set "filePath=%myPath%%jarPath%"
-java -jar %filePath% %*
+java -jar "%filePath%" %*
